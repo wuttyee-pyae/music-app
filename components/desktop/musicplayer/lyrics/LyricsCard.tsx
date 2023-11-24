3f8f5d1ff9213f863cfc9e9d6b55c04c404114d4
kotmth <kotmth@gmail.com>
2023-11-24 12:25:30 +0630

    add Lyrics Design

---
+++ components/desktop/musicplayer/lyrics/LyricsCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {ScrollShadow} from "@nextui-org/react";
+import {Content} from "./Content";
+
+export default function LyricsCard() {
+  return (
+    <ScrollShadow hideScrollBar size={100} className="w-full h-[500px]">
+      <Content />
+    </ScrollShadow>
+  );
+}
---
+++ src/app/desktop/lyrics/page.tsx
@@ -0,0 +1,11 @@
+import LyricsCard from "@/components/desktop/musicplayer/lyrics/LyricsCard";
+
+export default function Lyrics() {
+  return (
+        <div className="pt-6">
+        <div className="mb-4 wapper mx-4">
+            <LyricsCard/>
+        </div>
+    </div>
+  );
+}
